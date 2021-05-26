e5910584eedf2f69a229ec5e89db36c3971b407b
matheus <matheusf.ccosta@hotmail.com>
2021-05-26 09:27:02 -0300

    new commit

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -2,7 +2,7 @@ export function RepositoryItem(props) {
     return (
         <li>
             <strong>{props.repository.name ?? 'Default'}</strong>
-            <p>{props.repository.descripiton}</p>
+            <p>{props.repository.description}</p>
 
             <a href={props.repository.link}>
                 Access repository
